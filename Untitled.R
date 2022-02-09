0b9d43c9871f071872e47012c534baba9a6e0904
jasmine-hart <97899935+jasmine-hart@users.noreply.github.com>
2022-02-09 15:02:22 -0500

    upload hw4

---
+++ Untitled.R
@@ -0,0 +1,18 @@
+# Bio381 Week 4: Vectors & such
+# 9 feb 2022
+
+# Vectors rveiew -----------
+# type numeric/integer/double
+
+a <- c(10:30)
+
+# type character
+b <- c("carrots", "lettuce", "tomatoes")
+
+# binary/boolean/logical type
+c <- c(TRUE, TRUE, FALSE)
+c <- c(T, T, F)
+
+typeof(c)
+
+d <- c(4, 8.1, "yellow", FALSE)
